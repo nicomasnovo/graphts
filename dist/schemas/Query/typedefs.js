24d9545d58d839cb6559f8e3ae9a17cf132822c4
nicomasnovo <nicomasnovo@gmail.com>
2022-11-28 11:22:34 -0300

    🔥 remove dist files

--- dist/schemas/Query/typedefs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.querySchema = void 0;
-exports.querySchema = `
-  type Query {
-    healthcheck: String
-  }
-`;
--- dist/schemas/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.typeDefs = void 0;
-const typedefs_1 = require("./Books/typedefs");
-const typedefs_2 = require("./Movies/typedefs");
-exports.typeDefs = [typedefs_1.bookTypeDefs, typedefs_2.moviesSchema];
